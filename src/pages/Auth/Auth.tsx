78326d310f80ac39297b34b3138dff16afd0b63c
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2025-03-12 23:01:11 +0500

    Sixteenth Commit

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 import { debounce } from "lodash";
 import { useFormik } from "formik";
 import Button from "../../components/Button/Button";
-import React, { FC, useState, useEffect, useCallback } from "react";
+import React, { FC, useState, useCallback } from "react";
 import InputField from "../../components/Input/InputField/InputField";
 import { FlipCard, FlipCardBackSide, FlipCardFrontSide } from "../../components/FlipCard/FlipCard";
 import Separator from "../../components/Separator/Separator";
@@ -20,27 +20,14 @@ const validationSchema1 = Yup.object({
 });
 
 const validationSchema2 = Yup.object({
-  username: Yup.string().required("Username is required").min(3, "Must be at least 3 characters"),
-  email: Yup.string().email("Invalid email format").required("Email is required"),
-  password: Yup.string()
-    .required("Password is required")
-    .min(8, "Must be 8+ characters")
-    .matches(/[A-Z]/, "Include an uppercase letter")
-    .matches(/[a-z]/, "Include a lowercase letter")
-    .matches(/[0-9]/, "Include a number")
-    .matches(/[^A-Za-z0-9]/, "Include a special character"),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password")], "Passwords must match")
-    .required("Confirm Password is required"),
+  fullname: Yup.string().required("Fullname is required").min(3, "Must be at least 3 characters"),
+  email: Yup.string().email("Invalid email format").required("Email is required")
 });
 
 const Auth: FC = () => {
 
   const [changeSection, setChangeSection] = useState(true);
-
-  useEffect(() => {
-    document.getElementsByClassName("display-window")[0].scrollTop = 200;
-  }, []);
+  const [next, setNext] = useState(false);
 
   const form1 = useFormik({
     initialValues: { email: "", password: "" },
@@ -53,7 +40,7 @@ const Auth: FC = () => {
   });
 
   const form2 = useFormik({
-    initialValues: { username: "", email: "", password: "", confirmPassword: "" },
+    initialValues: { fullname: "", email: "" },
     validationSchema: validationSchema2,
     validateOnBlur: false,
     validateOnChange: false,
@@ -120,59 +107,50 @@ const Auth: FC = () => {
           </div>
         </div>
       </FlipCardFrontSide>
-
       <FlipCardBackSide>
-        <h1 className="logo">Kitaab</h1>
-        <form className="form" onSubmit={form2.handleSubmit}>
-          <div className="display-window">
-            <InputField
-              name="username"
-              title="Username"
-              placeholder="John Doe"
-              leftIcon="fa-user"
-              isScrollbar
-              onChange={handleChangeWithDebounce(form2)}
-              value={form2.values.username}
-              error={form2.touched.username ? form2.errors.username : undefined}
-            />
-            <InputField
-              name="email"
-              title="Email"
-              placeholder="john.doe@example.com"
-              leftIcon="fa-envelope"
-              isScrollbar
-              onChange={handleChangeWithDebounce(form2)}
-              value={form2.values.email}
-              error={form2.touched.email ? form2.errors.email : undefined}
-            />
-            <InputField
-              name="password"
-              title="Password"
-              isPassword
-              isScrollbar
-              onChange={handleChangeWithDebounce(form2)}
-              value={form2.values.password}
-              error={form2.touched.password ? form2.errors.password : undefined}
-            />
-            <InputField
-              name="confirmPassword"
-              title="Confirm Password"
-              isPassword
-              isScrollbar
-              onChange={handleChangeWithDebounce(form2)}
-              value={form2.values.confirmPassword}
-              error={form2.touched.confirmPassword ? form2.errors.confirmPassword : undefined}
-            />
+        <div className="flip-container">
+          <div className={`flip-content ${changeSection ? '': 'change-flip-section'}`}>
+            <div className="flip-section">
+              <h1 className="logo">Kitaab</h1>
+              <form className="form" onSubmit={form2.handleSubmit}>
+                <InputField
+                  name="fullname"
+                  title="Full Name"
+                  placeholder="John Doe"
+                  leftIcon="fa-user"
+                  onChange={handleChangeWithDebounce(form2)}
+                  value={form2.values.fullname}
+                  error={form2.touched.fullname ? form2.errors.fullname : undefined}
+                />
+                <InputField
+                  name="email"
+                  title="Email"
+                  placeholder="john.doe@example.com"
+                  leftIcon="fa-envelope"
+                  onChange={handleChangeWithDebounce(form2)}
+                  value={form2.values.email}
+                  error={form2.touched.email ? form2.errors.email : undefined}
+                />
+                <Button>
+                  <div onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)} onClick={()=> setChangeSection(false)}>
+                    Next
+                    <i className={`fa-solid fa-angles-right fa-xs next ${next ? 'animate-next': ''}`}></i>
+                  </div>
+                </Button>
+              </form>
+              <Separator/>
+              <p className="change-side">
+                Already have an account ?
+                <button className="link" data-flip-action>
+                  Login
+                </button>
+              </p>
+            </div>
+            <div className="flip-section">
+              <h1 className="logo" onClick={()=> setChangeSection(true)}>Kitaab</h1>
+            </div>
           </div>
-          <Button>Signup</Button>
-        </form>
-        <Separator/>
-        <p className="change-side">
-          Already have an account ?
-          <button className="link" data-flip-action>
-            Login
-          </button>
-        </p>
+        </div>
       </FlipCardBackSide>
     </FlipCard>
   );
